94d9be74c2dfe27e7c9c70f5cc5fe46665e204be
Nitish Sharma <niteshsharma5740@gmail.com>
2025-10-04 21:09:07 +0530

    Backend check and back to local

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -1,11 +1,12 @@
 import { io } from "socket.io-client";
 
-// const socket = io("http://localhost:5000", {
-//   transports: ["websocket"],
-// });
-// export default socket;
-
-const socket = io("https://share-a-three.vercel.app", {
+const socket = io("http://localhost:5000", {
   transports: ["websocket"],
 });
-export default socket;
+
+// const socket = io("https://share-a-backend.vercel.app", {
+//     transports: ["websocket"],
+// });
+
+
+  export default socket;
