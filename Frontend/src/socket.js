7498a58a735fdffdcbc2d04d28c8f34e0288cb8f
Nitish Sharma <niteshsharma5740@gmail.com>
2025-10-05 11:03:00 +0530

    Backend url fetched

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -1,12 +1,12 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
-  transports: ["websocket"],
-});
-
-// const socket = io("https://share-a-backend.vercel.app", {
-//     transports: ["websocket"],
+// const socket = io("http://localhost:5000", {
+//   transports: ["websocket"],
 // });
+
+const socket = io("https://sharea-backend.onrender.com", {
+    transports: ["websocket"],
+});
 
 
   export default socket;
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:5000",
-      // "/api":"https://share-a-backend.vercel.app",
+      // "/api": "http://localhost:5000",
+      "/api":"https://sharea-backend.onrender.com",
     },
   },
 })
